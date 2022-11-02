685167bf19e8e2fcb5a6ff6c6f5928d4f3053fa8
vankata05 <101401373+vankata05@users.noreply.github.com>
2022-11-02 19:23:34 +0200

    Implemented dual core movements

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,12 +7,16 @@ led = Pin("LED", Pin.OUT, value = 1)
 
 mv = CoreXY(0, 0);
 
-mv.G0(100, 100)
+mv.g0(200, 220)
 
 #0.9: max steps/s - 20000
 #1.8: max steps/s - 10000
     
-#while 1:
+while 1:
+    mv.g0(0, 0)
+    time.sleep(0.1)
+    mv.g0(220*8, 0)
+    time.sleep(0.1)
     #_thread.start_new_thread(trd, ())
     #tmc2209.step(3200, 1, 1)
     #a4988.step(800, 0, 1.17074)
--- MicroPython/movement.py
+++ MicroPython/movement.py
@@ -1,35 +1,36 @@
 from stepper import Stepper
+from _thread import start_new_thread
 
 tmc2209 = Stepper(18, 19)
 a4988 = Stepper(17, 16)
 
-def moveA(steps, dir_):
-    tmc2209.step(steps*8, dir_, 1)
+def moveA(steps, time):
+    if steps > 0:
+        tmc2209.step(steps*8, 1, time)
+    elif steps < 0:
+        tmc2209.step(-steps*8, 0, time)
     
-def moveB(steps, dir_):
-    a4988.step(steps, dir_, 1)
+def moveB(steps, time):
+    if steps > 0:
+        a4988.step(steps*2, 0, time)
+    elif steps < 0:
+        a4988.step(-steps*2, 1, time)
 
 class CoreXY:
     def __init__(self, x, y):
         self.x = x
         self.y = y
  
-    def G0(self, x, y):
+    def g0(self, x, y):
         stpA = (x - self.x) + (y - self.y)
         stpB = (x - self.x) - (y - self.y)
+        time = (abs(stpA) + abs(stpB))/(10000/8)
         
         print(stpA)
         print(stpB)
         
-        if stpA > 0:
-            moveA(stpA, 1)
-        elif stpA < 0:
-            moveA(stpA*-1, 0)
-        
-        if stpB > 0:
-            moveB(stpB, 1)
-        elif stpB< 0:
-            moveB(stpB*-1, 0)
+        start_new_thread(moveB, (stpB, time))
+        moveA(stpA, time)
             
         self.x = x
         self.y = y
